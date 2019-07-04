e03d35591c7fb2c46226de47b8d995679368e88f
Valentin Chizhenko <chigenko_v_m@mail.ru>
2019-07-04 19:57:53 +0300

    Добавил структуру

---
+++ MyFirstTestTask/src/main/java/com/valention/MyFirstTestTask/MyFirstTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.valention.MyFirstTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstTestTaskApplication.class, args);
+	}
+
+}
